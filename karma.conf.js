77afc672c9933f5b3792e91fe6b24693e0b7107f
Matthias Osswald <mat.osswald@sap.com>
2019-04-05 09:03:02 +0200

    Switch to karma-ui5

--- karma.conf.js
+++ karma.conf.js
@@ -5,45 +5,7 @@ module.exports = function(config) {
 
 	config.set({
 
-		basePath: 'webapp',
-
-		frameworks: ['qunit', 'openui5'],
-
-		openui5: {
-			path: 'http://localhost:8080/resources/sap-ui-core.js',
-			useMockServer : true
-
-		},
-
-		client: {
-			openui5: {
-				config: {
-					theme: 'sap_belize',
-					language: 'EN',
-					animation: false,
-					compatVersion: 'edge',
-					async: true,
-					resourceroots: {'sap.ui.demo.masterdetail': './base'}
-				},
-				tests: [
-					'sap/ui/demo/masterdetail/test/unit/AllTests',
-					'sap/ui/demo/masterdetail/test/integration/AllJourneys'
-				],
-				mockserver: {
-					metadataURL: '/base/localService/metadata.xml'
-				}
-			},
-			clearContext: false,
-			qunit: {
-				showUI: true
-			}
-		},
-
-		files: [
-			{ pattern: '**', included: false, served: true, watched: true},
-			"http://localhost:8080/resources/sap/ui/thirdparty/sinon.js",
-			"http://localhost:8080/resources/sap/ui/thirdparty/sinon-qunit.js"
-		],
+		frameworks: ['ui5'],
 
 		reporters: ['progress'],
 
