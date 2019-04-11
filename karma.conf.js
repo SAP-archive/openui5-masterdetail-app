81bcdeaf66514ddac162ca76529e85ec8a66ab23
Matthias Osswald <mat.osswald@sap.com>
2019-04-11 10:12:30 +0200

    Set exact screen size for Karma Testing
    Chrome and Chrome Headless use a different default screen size.
    This caused the tests to fail in Headless, as the screen was only
    800px wide.
    To use the same size in both variants is it passed as CLI argument.

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,10 @@
 module.exports = function(config) {
 	"use strict";
 
+	var chromeFlags = [
+		"--window-size=1280,1024"
+	];
+
 	config.set({
 
 		frameworks: ['ui5'],
@@ -17,7 +21,18 @@ module.exports = function(config) {
 
 		autoWatch: true,
 
-		browsers: ['Chrome'],
+		customLaunchers: {
+			CustomChrome: {
+				base: "Chrome",
+				flags: chromeFlags
+			},
+			CustomChromeHeadless: {
+				base: "ChromeHeadless",
+				flags: chromeFlags
+			}
+		},
+
+		browsers: ['CustomChrome'],
 
 		singleRun: false
 	});
