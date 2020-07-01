efe4529c0f84a8fae56f2888012905ea95b75957
flovogt <flovogt@users.noreply.github.com>
2020-07-01 09:12:04 +0000

    deploy: 5acaebf636ff78b91857b0e38ae9919d5237b626

--- test/initMockServer-dbg.js
+++ test/initMockServer-dbg.js
@@ -1,7 +1,6 @@
 sap.ui.define([
-	"../localService/mockserver",
-	"sap/m/MessageBox"
-], function (mockserver, MessageBox) {
+	"../localService/mockserver"
+], function (mockserver) {
 	"use strict";
 
 	var aMockservers = [];
@@ -10,7 +9,15 @@ sap.ui.define([
 	aMockservers.push(mockserver.init());
 
 	Promise.all(aMockservers).catch(function (oError) {
-		MessageBox.error(oError.message);
+		var fnShowErrorMessage = function () {
+			return new Promise(function (resolve, reject) {
+				sap.ui.require(["sap/m/MessageBox"], function (MessageBox) {
+					MessageBox.error(oError.message);
+					resolve();
+				}, reject);
+			});
+		}
+		return sap.ui.getCore().loadLibrary("sap.m", { async: true }).then(fnShowErrorMessage);
 	}).finally(function () {
 		// initialize the embedded component on the HTML page
 		sap.ui.require(["sap/ui/core/ComponentSupport"]);
